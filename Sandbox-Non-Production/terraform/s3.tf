0f24762cb0167193fa3c4e0adf5bf338400009fc
Sundar M <you@example.com>
2024-11-28 17:24:38 +0530

    updated sbx non -production

--- Sandbox-Non-Production/terraform/s3.tf
+++ Sandbox-Non-Production/terraform/s3.tf
@@ -2,5 +2,4 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
 }
